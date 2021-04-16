69a567e4b9c6f882263947b3484b54ce97ad29ef
angel gutarra <agutarr2@gmu.edu>
2021-04-16 14:50:34 -0400

    more transformations

--- irbem_tests/geopack_08/simple_python_test/geopack_spacepy_comparison.py
+++ irbem_tests/geopack_08/simple_python_test/geopack_spacepy_comparison.py
@@ -18,88 +18,105 @@ from matplotlib import pyplot as plt
 debug = False
 
 # options: SMGSW_08_V , GEOMAG_08_V, 
-# to be made: SPHCAR_08_V, GEIGEO_08_V, 
-# MAGSM_08_V, GEOGSW_08_V, GEODGEO_08_V ** this one will be strange
-# done but not tested: GSWGSE_08_V,
-wrapper_subroutine_name='GEOMAG_08_V'
-# allowable timeframe 1965-2020
-IYEAR = 1997
-IDAY = 1
-IHOUR = 0
-MIN = 0
-ISEC = 0
+# to be made: SPHCAR_08_V, 
+#   GEODGEO_08_V ** this one will be strange
+
 
 # controls the direction of tranformation
 # generic_tranformation(X1,Y1,Z1,X2,Y2,Z2,J)
 # if J>0, then X1,Y1,Z1 -> X2, Y2,Z2
 # if J<0, then X1,Y1,Z1 <- X2, Y2,Z2
 J = 1 # test out 1 and -1 for both sc and gp
-arr_size = 10**1
-X1 = np.arange(0,arr_size,dtype=np.float64)
-Y1 = np.arange(0,arr_size,dtype=np.float64)
-Z1 = np.arange(0,arr_size,dtype=np.float64)
-# X2 = np.arange(0,5,dtype=np.float64)
-# Y2 = np.arange(0,5,dtype=np.float64)
-# Z2 = np.arange(0,5,dtype=np.float64)
-X2 = np.zeros(arr_size, dtype=np.float64)
-Y2 = np.zeros(arr_size, dtype=np.float64)
-Z2 = np.zeros(arr_size, dtype=np.float64)
-
-N = X1.size
-
-with open(wrapper_subroutine_name+'.txt','r') as f:
-    wrapper_script = ''.join(f.readlines())
-
-geomag_08_V_F = jitFORTRAN.Fortran_Subroutine(wrapper_script,
-                                              wrapper_subroutine_name,
-                                              include='geopack_08')
-
-geomag_08_V_F.execute(X1,Y1,Z1,X2,Y2,Z2,
-                    J,
-                    IYEAR,IDAY,IHOUR,MIN,ISEC,
-                    N)
-
-time = "{0:04} {1:03d} {2:02d} {3:02d} {4:02d}".format(IYEAR, IDAY, IHOUR, MIN, ISEC)
-time = datetime.strptime(time, "%Y %j %H %M %S")
-time = [time.year,time.month, time.day, time.hour,time.minute,time.second]
-
-# print('\n     i got git\n')
-# print(J)
-if J>0:
-    points = np.column_stack((X1,Y1,Z1))
-    gp_points = np.column_stack((X2,Y2,Z2))
-    if wrapper_subroutine_name=='SMGSW_08_V':
-        sc_points = cx.SMtoGSM(points,time,'car','car')
-        title='SM to GSW'
-    elif wrapper_subroutine_name=='GEOMAG_08_V':
-        sc_points = cx.GEOtoMAG(points,time,'car','car')
-        title='GEO to MAG'
-else:
-    # print('\n     i got git\n')
-    points = np.column_stack((X2,Y2,Z2))
-    gp_points = np.column_stack((X1,Y1,Z1))
-    if wrapper_subroutine_name=='SMGSW_08_V':
-        # print('i got hit!\n')
-        sc_points = cx.GSMtoSM(points,time,'car','car')
-        title='GSM to SM'
-    elif wrapper_subroutine_name=='GEOMAG_08_V':
-        sc_points = cx.MAGtoGEO(points,time,'car','car')
-        title='MAG to GEO'
+# done but not tested: GSWGSE_08_V, GEIGEO_08_V, MAGSM_08_V, GEOGSW_08_V
+wrap_sub_names = ['SMGSW_08_V','GEOMAG_08_V','GSWGSE_08_V','GEIGEO_08_V',
+                     'MAGSM_08_V','GEOGSW_08_V']
+for wrap_sub_name in wrap_sub_names:
+    # allowable timeframe 1965-2020
+    IYEAR = 1997
+    IDAY = 1
+    IHOUR = 0
+    MIN = 0
+    ISEC = 0
     
-# plt.plot(points[:,0],sc_points[:,0] - gp_points[:,0])
-# plt.title(title + ' x-axis')
-# plt.xlabel('1 unit')
-# plt.ylabel('spacepy - geopack x-axis')
-# plt.show()
+    
 
-# plt.plot(points[:,1],sc_points[:,1] - gp_points[:,1])
-# plt.title(title + ' y-axis')
-# plt.xlabel('1 unit')
-# plt.ylabel('spacepy - geopack y-axis')
-# plt.show()
+    arr_size = 10**1
+    X1 = np.arange(0,arr_size,dtype=np.float64)
+    Y1 = np.arange(0,arr_size,dtype=np.float64)
+    Z1 = np.arange(0,arr_size,dtype=np.float64)
 
-# plt.plot(points[:,2],sc_points[:,2] - gp_points[:,2])
-# plt.title(title + ' z-axis')
-# plt.xlabel('1 unit')
-# plt.ylabel('spacepy - geopack z-axis')
-# plt.show()
+    X2 = np.zeros(arr_size, dtype=np.float64)
+    Y2 = np.zeros(arr_size, dtype=np.float64)
+    Z2 = np.zeros(arr_size, dtype=np.float64)
+    
+    N = X1.size
+    
+    with open(wrap_sub_name +'.txt','r') as f:
+        wrapper_script = ''.join(f.readlines())
+    
+    geomag_08_V_F = jitFORTRAN.Fortran_Subroutine(wrapper_script,
+                                                  wrap_sub_name,
+                                                  include='geopack_08')
+    
+    geomag_08_V_F.execute(X1,Y1,Z1,X2,Y2,Z2,
+                        J,
+                        IYEAR,IDAY,IHOUR,MIN,ISEC,
+                        N)
+    
+    time = "{0:04} {1:03d} {2:02d} {3:02d} {4:02d}".format(IYEAR, IDAY, IHOUR, MIN, ISEC)
+    time = datetime.strptime(time, "%Y %j %H %M %S")
+    time = [time.year,time.month, time.day, time.hour,time.minute,time.second]
+    
+    # print('\n     i got git\n')
+    # print(J)
+    if J>0:
+        points = np.column_stack((X1,Y1,Z1))
+        gp_points = np.column_stack((X2,Y2,Z2))
+
+        if wrap_sub_name=='SMGSW_08_V':
+            sc_points = cx.SMtoGSM(points,time,'car','car')
+            title='SM to GSW'
+        elif wrap_sub_name=='GEOMAG_08_V':
+            sc_points = cx.GEOtoMAG(points,time,'car','car')
+            title='GEO to MAG'
+        elif wrap_sub_name=='GSWGSE_08_V':
+            sc_points = cx.GSMtoGSE(points,time,'car','car')
+            title='GSM to GSE'
+        elif wrap_sub_name=='GEIGEO_08_V':
+            sc_points = cx.GEItoGEO(points,time,'car','car')
+            title='GEI to GEO'
+        elif wrap_sub_name=='MAGSM_08_V':
+            sc_points = cx.MAGtoSM(points,time,'car','car')
+            title='MAG to SM'
+        elif wrap_sub_name=='GEOGSW_08_V':
+            sc_points = cx.GEOtoGSW(points,time,'car','car')
+            title='GEO to GSW'
+    else:
+        # print('\n     i got git\n')
+        points = np.column_stack((X2,Y2,Z2))
+        gp_points = np.column_stack((X1,Y1,Z1))
+        if wrap_sub_name=='SMGSW_08_V':
+            # print('i got hit!\n')
+            sc_points = cx.GSMtoSM(points,time,'car','car')
+            title='GSM to SM'
+        elif wrap_sub_name=='GEOMAG_08_V':
+            sc_points = cx.MAGtoGEO(points,time,'car','car')
+            title='MAG to GEO'
+        
+    plt.plot(points[:,0],sc_points[:,0] - gp_points[:,0])
+    plt.title(title + ' x-axis')
+    plt.xlabel('1 unit')
+    plt.ylabel('spacepy - geopack x-axis')
+    plt.show()
+    
+    plt.plot(points[:,1],sc_points[:,1] - gp_points[:,1])
+    plt.title(title + ' y-axis')
+    plt.xlabel('1 unit')
+    plt.ylabel('spacepy - geopack y-axis')
+    plt.show()
+    
+    plt.plot(points[:,2],sc_points[:,2] - gp_points[:,2])
+    plt.title(title + ' z-axis')
+    plt.xlabel('1 unit')
+    plt.ylabel('spacepy - geopack z-axis')
+    plt.show()
